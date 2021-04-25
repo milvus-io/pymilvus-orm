74cca4d267453befe8b46d031319e87b39903c88
cai.zhang <cai.zhang@zilliz.com>
2021-04-25 16:49:37 +0800

    Add create index test for binary data (#56)
    Signed-off-by: xiaocai2333 <cai.zhang@zilliz.com>

--- examples/collection.py
+++ examples/collection.py
@@ -14,14 +14,15 @@ from pymilvus_orm.connections import connections
 from pymilvus_orm.schema import FieldSchema, CollectionSchema
 from pymilvus_orm.types import DataType
 import random
+import numpy as np
 from sklearn import preprocessing
 import string
 
 default_dim = 128
 default_nb = 3000
 default_float_vec_field_name = "float_vector"
+default_binary_vec_field_name = "binary_vector"
 default_segment_row_limit = 1000
-
 
 
 all_index_types = [
@@ -58,9 +59,10 @@ default_index_params = [
 
 
 default_index = {"index_type": "IVF_FLAT", "params": {"nlist": 128}, "metric_type": "L2"}
+default_binary_index = {"index_type": "BIN_FLAT", "params": {"nlist": 1024}, "metric_type": "JACCARD"}
 
 
-def gen_default_fields(auto_id=True):
+def gen_default_fields():
     default_fields = [
         FieldSchema(name="int64", dtype=DataType.INT64, is_primary=False),
         FieldSchema(name="float", dtype=DataType.FLOAT),
@@ -71,18 +73,49 @@ def gen_default_fields(auto_id=True):
     return default_schema
 
 
-def gen_vectors(num, dim, is_normal=True):
+def gen_binary_schema():
+    binary_fields = [
+        FieldSchema(name="int64", dtype=DataType.INT64, is_primary=False),
+        FieldSchema(name="float", dtype=DataType.FLOAT),
+        FieldSchema(name=default_binary_vec_field_name, dtype=DataType.BINARY_VECTOR, dim=default_dim)
+    ]
+    default_schema = CollectionSchema(fields=binary_fields, description="test collection",
+                                      segment_row_limit=default_segment_row_limit, auto_id=True)
+    return default_schema
+
+
+def gen_float_vectors(num, dim, is_normal=True):
     vectors = [[random.random() for _ in range(dim)] for _ in range(num)]
     vectors = preprocessing.normalize(vectors, axis=1, norm='l2')
     return vectors.tolist()
 
 
-def gen_data(nb, is_normal=False):
-    vectors = gen_vectors(nb, default_dim, is_normal)
+def gen_float_data(nb, is_normal=False):
+    vectors = gen_float_vectors(nb, default_dim, is_normal)
     entities = [
         [i for i in range(nb)],
         [float(i) for i in range(nb)],
         vectors
+    ]
+    return entities
+
+
+def gen_binary_vectors(num, dim):
+    raw_vectors = []
+    binary_vectors = []
+    for i in range(num):
+        raw_vector = [random.randint(0, 1) for i in range(dim)]
+        raw_vectors.append(raw_vector)
+        binary_vectors.append(bytes(np.packbits(raw_vector, axis=-1).tolist()))
+    return raw_vectors, binary_vectors
+
+
+def gen_binary_data(nb):
+    raw_vectors, binary_vectors = gen_binary_vectors(nb, dim=default_dim)
+    entities = [
+        [i for i in range(nb)],
+        [float(i) for i in range(nb)],
+        binary_vectors
     ]
     return entities
 
@@ -112,8 +145,8 @@ connections.create_connection(alias="default")
 
 def test_create_collection():
     collection = Collection(name=gen_unique_str(), schema=gen_default_fields())
-    print(collection.name)
-    print(collection.description)
+    assert collection.is_empty is True
+    assert collection.num_entities == 0
     collection.drop()
 
 
@@ -121,7 +154,7 @@ def test_collection_only_name():
     name = gen_unique_str()
     collection_temp = Collection(name=name, schema=gen_default_fields())
     collection = Collection(name=name)
-    data = gen_data(default_nb)
+    data = gen_float_data(default_nb)
     collection.insert(data)
     collection.load()
     assert collection.is_empty is False
@@ -130,7 +163,7 @@ def test_collection_only_name():
 
 
 def test_collection_with_data():
-    data = gen_data(default_nb)
+    data = gen_float_data(default_nb)
     collection = Collection(name=gen_unique_str(), data=data, schema=gen_default_fields())
     collection.load()
     assert collection.is_empty is False
@@ -139,10 +172,19 @@ def test_collection_with_data():
 
 
 def test_create_index_float_vector():
-    data = gen_data(default_nb)
+    data = gen_float_data(default_nb)
     collection = Collection(name=gen_unique_str(), data=data, schema=gen_default_fields())
     for index_param in gen_simple_index():
         collection.create_index(field_name=default_float_vec_field_name, index_params=index_param)
+    assert len(collection.indexes) != 0
+    collection.drop()
+
+
+def test_create_index_binary_vector():
+    collection = Collection(name=gen_unique_str(), schema=gen_binary_schema())
+    data = gen_binary_data(default_nb)
+    collection.insert(data)
+    collection.create_index(field_name=default_binary_vec_field_name, index_params=default_binary_index)
     assert len(collection.indexes) != 0
     collection.drop()
 
@@ -151,3 +193,4 @@ test_create_collection()
 test_collection_only_name()
 test_collection_with_data()
 test_create_index_float_vector()
+test_create_index_binary_vector()
