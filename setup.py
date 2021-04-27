035354e43a49b2a4e9d308107b50f0bd7bf58f52
cai.zhang <cai.zhang@zilliz.com>
2021-04-27 15:16:03 +0800

    Add requirements for installing and add hello_milvus
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ README = (HERE / 'README.md').read_text()
 with io.open("pymilvus_orm/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
+requirements = []
+with open("requirements.txt", 'r') as f:
+    for line in f.readlines():
+        requirements.append(line.strip())
+
 setuptools.setup(
     name="pymilvus-orm",
     version=version,
@@ -20,7 +25,7 @@ setuptools.setup(
     license="Apache-2.0",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["requests>=2.22.0", "pymilvus-distributed>=0.0.62"],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
