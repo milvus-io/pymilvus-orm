f95b125b82907ea89b51c8689307a71d47f2e9b1
cai.zhang <cai.zhang@zilliz.com>
2021-06-30 11:16:07 +0800

    Collection has no parameter of data (#214)
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- examples/hello_milvus.py
+++ examples/hello_milvus.py
@@ -32,7 +32,7 @@ def hello_milvus():
     default_schema = schema.CollectionSchema(fields=default_fields, description="test collection")
 
     print(f"\nCreate collection...")
-    collection = Collection(name="hello_milvus", data=None, schema=default_schema)
+    collection = Collection(name="hello_milvus", schema=default_schema)
 
     print(f"\nList collections...")
     print(list_collections())
